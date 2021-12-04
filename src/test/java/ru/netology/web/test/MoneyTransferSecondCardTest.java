687e645b30b6681f84a3d7f35c7f94b801a68077
EvgeniaRepina <88374316+EvgeniaRepina@users.noreply.github.com>
2021-12-04 15:56:52 +0100

    done update

--- src/test/java/ru/netology/web/test/MoneyTransferSecondCardTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferSecondCardTest.java
@@ -117,6 +117,25 @@ class MoneyTransferSecondCardTest {
         int expectedFirstCard = preBalanceInfo.getBalanceFirstCard();
         int expectedSecondCard = preBalanceInfo.getBalanceSecondCard();
 
+        dashboardPage.setValue("138706");
+        dashboardPage.depositSecondCard(DataHelper.getFirstCardInfo());
+
+        DashboardPage dashboardPage2 = new DashboardPage();
+        int actualFirstCard = dashboardPage2.getCardBalance("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]");
+        int actualSecondCard = dashboardPage2.getCardBalance("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]");
+
+        assertEquals(expectedFirstCard, actualFirstCard);
+        assertEquals(expectedSecondCard, actualSecondCard);
+    }
+    @Test
+    void ShouldTopupSecondCard_EnormousValue_NotEmptyBalanceOfEachCard() {
+
+        DashboardPage dashboardPage = new DashboardPage();
+        PreBalanceInfo preBalanceInfo = new PreBalanceInfo();
+
+        int expectedFirstCard = preBalanceInfo.getBalanceFirstCard();
+        int expectedSecondCard = preBalanceInfo.getBalanceSecondCard();
+
         dashboardPage.setValue("8888888");
         dashboardPage.depositSecondCard(DataHelper.getFirstCardInfo());
 
